060f581329fd7b67673aa03a5fb10533b16d006f
Julian Betts <wjulianbetts@gmail.com>
2022-10-05 18:51:49 -0700

    new

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 
 
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 
 console.log("hey")
